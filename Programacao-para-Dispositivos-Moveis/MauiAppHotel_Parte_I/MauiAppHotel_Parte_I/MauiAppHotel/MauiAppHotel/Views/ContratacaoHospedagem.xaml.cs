c3838fe678d40ab5245cf11b4ea5a3413e7753a1
Fujimiya <142509204+Fuyimiya@users.noreply.github.com>
2026-05-30 08:15:19 -0300

    Add files via upload
    App Hotel parte 1

---
+++ Programacao-para-Dispositivos-Moveis/MauiAppHotel_Parte_I/MauiAppHotel_Parte_I/MauiAppHotel/MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,71 @@
+using MauiAppHotel.Models;
+
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+    List<Quarto> lista_quartos = new()
+    {
+        new Quarto()
+        {
+            Descricao = "Suíte Super Luxo",
+            ValorDiariaAdulto = 110.0,
+            ValorDiariaCrianca = 55
+        },
+        new Quarto()
+        {
+            Descricao = "Suíte Luxo",
+            ValorDiariaAdulto = 80.0,
+            ValorDiariaCrianca = 40
+        },
+        new Quarto()
+        {
+            Descricao = "Suíte Single",
+            ValorDiariaAdulto = 50.0,
+            ValorDiariaCrianca = 25.0
+        },
+        new Quarto()
+        {
+            Descricao = "Suíte Crise",
+            ValorDiariaAdulto = 25,
+            ValorDiariaCrianca = 12.5
+        },
+    };
+
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+        
+        // Abastecendo o picker com a lista de quartos.
+        pck_quarto.ItemsSource = lista_quartos;
+
+        // Validando a data mínima de máxima de checkin
+        dtpck_checkin.MinimumDate = DateTime.Now;
+        //dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+        dtpck_checkin.MaximumDate = DateTime.Now.AddMonths(2);
+
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.Value.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.Value.AddMonths(2);
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = (DatePicker)sender;
+
+        DateTime data_selecionada = elemento.Date.Value;
+
+        dtpck_checkout.MinimumDate = data_selecionada.AddDays(1);
+        dtpck_checkout.MaximumDate = data_selecionada.AddMonths(2);
+    }
+
+    private void dtpck_checkout_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
